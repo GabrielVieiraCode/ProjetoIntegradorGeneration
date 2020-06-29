0c9784cd388cc5e1c214fd8806949432b56f7dd7
Gabriel vieira <gabriel.vieira.trabalho@gmail.com>
2020-06-28 23:13:26 -0300

    Post comentario consumido

--- mestresdahorta/src/app/contato/contato.component.ts
+++ mestresdahorta/src/app/contato/contato.component.ts
@@ -3,6 +3,8 @@ import{faInstagram} from '@fortawesome/free-brands-svg-icons';
 import {faUser} from '@fortawesome/free-regular-svg-icons';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import {faPhone} from '@fortawesome/free-solid-svg-icons';
+import { Contato } from '../model/Contato';
+import {  ContatoService } from 'src/app/service/contato.service';
 
 @Component({
   selector: 'app-contato',
@@ -14,9 +16,18 @@ export class ContatoComponent implements OnInit {
 faUser = faUser
 faEnvelope = faEnvelope
 faPhone =faPhone
-  constructor() { }
+contato: Contato = new Contato;
+  constructor(private ContatoService:ContatoService) { }
 
   ngOnInit(): void {
+  }
+
+
+  publicar() {
+    this.ContatoService.postContato(this.contato).subscribe((resp: Contato) => {
+      this.contato = resp;
+      location.assign('/contato');
+    })
   }
 
 }
