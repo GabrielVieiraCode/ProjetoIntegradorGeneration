217d2a25c7416480b6a3fb574213c4397a4a4c8f
gustavofre <gustavolfre@gmail.com>
2020-07-05 14:37:55 -0300

    solução de merging

--- ecomerce/src/main/java/com/mestredahorta/ecomerce/controller/MensagensController.java
+++ ecomerce/src/main/java/com/mestredahorta/ecomerce/controller/MensagensController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mestredahorta.ecomerce.model.Mensagens;
+import com.mestredahorta.ecomerce.model.Usuario;
 import com.mestredahorta.ecomerce.repository.MensagemRepository;
 
 
